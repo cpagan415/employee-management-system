dc7a05ac59722128bbf5089bfcb27949dbd6e048
Christina Pagan <enterprise@christinas-mbp.home>
2021-05-15 23:51:55 -0400

    added function to add a role or employee to the database

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const db = require('./db/connect');
-const { debugPort } = require('process');
+const { exit } = require('process');
+const { start } = require('repl');
 require('console.table');
 
 
    
-
+//text art from https://fsymbols.com/text-art/
 function title() {    
-    console.log(" -----------------------------------------------------\n" +
-                "|   _______                                           |\n"+
-                "|  |     __|                                          |\n" +
-                "|  |    |__                                           |\n" +
-                "|  |     __|                                          |\n" +
-                "|  |    |__                                           |\n" +
-                "|  |_______|                                          |\n" +
-                "|                                                     |\n" +
-                "|                                                     |\n" +
-                "|                                                     |\n" +
-                "|                                                     |\n" +
-                "|                                                     |\n" +
-                "|                                                     |\n" +
-                "|                                                     |\n" +
-                " -----------------------------------------------------");
+    console.log(
+        "███████╗███╗░░░███╗██████╗░██╗░░░░░░█████╗░██╗░░░██╗███████╗███████╗\n" +
+        "██╔════╝████╗░████║██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔════╝\n" +
+        "█████╗░░██╔████╔██║██████╔╝██║░░░░░██║░░██║░╚████╔╝░█████╗░░█████╗░░\n" +
+        "██╔══╝░░██║╚██╔╝██║██╔═══╝░██║░░░░░██║░░██║░░╚██╔╝░░██╔══╝░░██╔══╝░░\n"+
+        "███████╗██║░╚═╝░██║██║░░░░░███████╗╚█████╔╝░░░██║░░░███████╗███████╗\n"+
+        "╚══════╝╚═╝░░░░░╚═╝╚═╝░░░░░╚══════╝░╚════╝░░░░╚═╝░░░╚══════╝╚══════╝\n"+
+        
+        "████████╗██████╗░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░\n"+
+        "╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗\n"+
+        "░░░██║░░░██████╔╝███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝\n"+
+        "░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗\n"+
+        "░░░██║░░░██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║\n"+
+        "░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n");
 }
 
-
+title();
 
 //queries here and maybe would send to another file 
 function allDept()
@@ -86,13 +86,69 @@ const addDept = function()
     })
 }
 
+const addRole = function()
+{
+    inquirer.prompt([
+        {
+            name: 'roleTitle',
+            type: 'input',
+            message: 'Enter the name of the position'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter salary amount: '   //add validation for decimal, decimal is automatically placed in sql chart
+        },
+        {
+            name: 'deptId',
+            type: 'input',
+            message: 'Enter the Department Id:' //validation that dept ID is a number between the listed department ID range, does the department exist?
+        }
+
+    ]).then(answer =>{
+        console.log(answer.salary + ' '+ answer.deptId);
+        db.query(`INSERT INTO roles (job_title, salary, dept_id) VALUES ('${answer.roleTitle}', ${answer.salary}, ${answer.deptId});`, (err, data)=>{
+           startProgram();
+        })
+    })
+}
+
+const addEmply = function()
+{
+    inquirer.prompt([
+        {
+            name:'firstName',
+            type:'input',
+            message:'New employee first name:'
+        },
+        {
+            name:'lastName',
+            type:'input',
+            message:'New employee last name:'
+        },
+        {
+            name: 'roleId',
+            type: 'input',
+            message: 'Enter role id number for new employee: '//validation here for rold id, make sure it is not out of range   
+        },
+        {
+            name: 'managerId',
+            type: 'input',
+            message: 'Enter manager id for new employee:'//validation again here to check range, depending on number maybe add <manager> was added.
+        }
+    ]).then(answer =>{
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answer.firstName}', '${answer.lastName}', ${answer.roleId}, ${answer.managerId});`, (err, data) =>{
+            startProgram();
+        })
+    })
+}
 
 const appChoice = [
     {
         name: 'appChoice',
         type: 'list',
         message: 'What would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit']
         }
 ];
 
@@ -115,6 +171,15 @@ const appChoice = [
             case 'Add a Department':
                 addDept();
                 break;
+            case 'Add a Role':
+                addRole();
+                break;
+            case 'Add an Employee':
+                addEmply();
+                break;
+           case 'Exit':
+               console.log('End of Program');
+                break;
         }
     })
     .catch(error =>{
@@ -125,5 +190,5 @@ const appChoice = [
     })
 }
 
-//title();
+
 startProgram();
